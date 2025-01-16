985f1365792da9926131c18b79145ff87cecfd8e
ayachi <1728353049@qq.com>
2025-01-16 14:44:44 +0800

    Add files via upload

--- black_holes_search.py
+++ black_holes_search.py
@@ -1,8 +1,8 @@
 import numpy as np
 for n in range(10):
+    n = n+1
     for b in range(10):
-        n += 1
-        b += 1
+        b = b+1
         a1 = np.random.randint(0, b, [1, n])
         l1 = a1.tolist()[0]
         l1.sort(reverse=True)
@@ -21,11 +21,11 @@ for n in range(10):
                         a2 = (c - c % pow(b, n - x - 1)) / pow(b, n - x - 1)
                         l2.append(int(a2))
                         c = c % pow(b, n - x - 1)
-                    print('number of number：',n,'and', b, 'th system:', 'success!,black hole is', l2, l0, file=file)
+                    print('number of number：',n,'and', b, 'th system:', 'success!, the black hole is', l2, l0, file=file)
                     break
             elif max in l0 and max != l0[len(l0) - 1]:
                 with open('number_balck_holes_search.txt', 'a', newline='\n') as file:
-                    print('number of number：',n,'and', b, 'th system:', 'fail,we do not find black hole', l0, file=file)
+                    print('number of number：',n,'and', b, 'th system:', 'fail,we do not find the black hole', l0, file=file)
                     break
             else:
                 l0.append(max)
